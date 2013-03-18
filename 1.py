c045019bf3230c7987192b9a3e966f0ae629bb02
djhang <dujinhang@gmail.com>
2013-03-18 14:41:58 +0800

    recommend.py

---
+++ 1.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python
+#coding=utf-8
+from math import sqrt
+
+users = {"Angelica": {"Blues Traveler": 3.5, "Broken Bells": 2.0, "Norah Jones": 4.5, "Phoenix": 5.0, "Slightly Stoopid": 1.5, "The Strokes": 2.5, "Vampire Weekend": 2.0},
+         "Bill":{"Blues Traveler": 2.0, "Broken Bells": 3.5, "Deadmau5": 4.0, "Phoenix": 2.0, "Slightly Stoopid": 3.5, "Vampire Weekend": 3.0},
+         "Chan": {"Blues Traveler": 5.0, "Broken Bells": 1.0, "Deadmau5": 1.0, "Norah Jones": 3.0, "Phoenix": 5, "Slightly Stoopid": 1.0},
+         "Dan": {"Blues Traveler": 3.0, "Broken Bells": 4.0, "Deadmau5": 4.5, "Phoenix": 3.0, "Slightly Stoopid": 4.5, "The Strokes": 4.0, "Vampire Weekend": 2.0},
+         "Hailey": {"Broken Bells": 4.0, "Deadmau5": 1.0, "Norah Jones": 4.0, "The Strokes": 4.0, "Vampire Weekend": 1.0},
+         "Jordyn":  {"Broken Bells": 4.5, "Deadmau5": 4.0, "Norah Jones": 5.0, "Phoenix": 5.0, "Slightly Stoopid": 4.5, "The Strokes": 4.0, "Vampire Weekend": 4.0},
+         "Sam": {"Blues Traveler": 5.0, "Broken Bells": 2.0, "Norah Jones": 3.0, "Phoenix": 5.0, "Slightly Stoopid": 4.0, "The Strokes": 5.0},
+         "Veronica": {"Blues Traveler": 3.0, "Norah Jones": 5.0, "Phoenix": 4.0, "Slightly Stoopid": 2.5, "The Strokes": 3.0}
+        }
+
+
+def manhatten(rating1, rating2):
+    """Compute the distance between users[i] and users[j] """
+    distance = 0
+    num = 0
+    for key in rating1:
+        if key in rating2:
+            distance += abs(rating1[key] - rating2[key])
+            num += 1
+        
+    if flag > 0:
+        return distance / num
+    else:
+        return -1
+
+def NearestUser(username, users):
+    distances = []
+    for u in users:
+        if u != username:
+            dis = manhatten(users[username], users[u])
+            distances.append((dis, u))
+    distances.sort()
+    return distances
+
+
+def recommend(username, users):
+    nearest = NearestUser(username, users)[0][1]
+    
+    
+
+
